7bd4d67924a7b7fee76058e863b3edf280c057d0
Tom O <thomasowca@yahoo.com>
2019-07-20 10:21:28 -0500

    Initial upload.

---
+++ Airline Web API/Airline Web API/Controllers/AircraftController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airline_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AircraftController : Controller
+    {
+        // GET: api/aircraft
+        [HttpGet]
+        public IActionResult Aircraft()
+        {
+            // Implement logic for retrieving aircraft in here.
+            return Ok();
+        }
+
+        // GET: api/aircraft
+        [HttpPost]
+        public IActionResult Aircraft([FromBody] Aircraft aircraft)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Airline Web API/Airline Web API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airline_Web_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //public DbSet<T> Name { get; set; }
+    }
+}
